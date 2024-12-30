4e2e9624b2f28431c4c3adb83d94962d5bd24d8a
WonHyung Shin <wonhyung64@icloud.com>
2024-12-30 21:16:56 +0900

    feat - finish esmm

--- interaction/esmm.py
+++ interaction/esmm.py
@@ -27,17 +27,29 @@ except:
 for seed in range(10):
     # SETTINGS
     parser = argparse.ArgumentParser()
-    parser.add_argument("--embedding-k", type=int, default=18)
-    parser.add_argument("--lr", type=float, default=1e-8)
-    parser.add_argument("--weight-decay", type=float, default=0.)
-    parser.add_argument("--batch-size", type=int, default=2048)
+
+    """coat"""
+    parser.add_argument("--embedding-k", type=int, default=128)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--weight-decay", type=float, default=1e-4)
+    parser.add_argument("--batch-size", type=int, default=4096)
+    parser.add_argument("--dataset-name", type=str, default="coat")
+
+    """yahoo"""
+    # parser.add_argument("--embedding-k", type=int, default=64)
+    # parser.add_argument("--lr", type=float, default=1e-3)
+    # parser.add_argument("--weight-decay", type=float, default=1e-6)
+    # parser.add_argument("--batch-size", type=int, default=8192)
+    # parser.add_argument("--dataset-name", type=str, default="yahoo_r3")
+
     parser.add_argument("--num-epochs", type=int, default=1000)
     parser.add_argument("--random-seed", type=int, default=seed)
     parser.add_argument("--evaluate-interval", type=int, default=50)
     parser.add_argument("--top-k-list", type=list, default=[1,3,5,7,10])
     parser.add_argument("--data-dir", type=str, default="./data")
-    parser.add_argument("--dataset-name", type=str, default="coat")
+
     parser.add_argument("--G", type=int, default=1)
+
     try:
         args = parser.parse_args()
     except:
