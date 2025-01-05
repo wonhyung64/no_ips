32dfbac06216388ea55434162bbdc9214a066d4a
WonHyung Shin <wonhyung64@icloud.com>
2025-01-05 18:35:33 +0900

    feat - add akbips

--- interaction/module/model.py
+++ interaction/module/model.py
@@ -190,3 +190,38 @@ class ESCM2Ips(nn.Module):
         out_ctcvr = torch.mul(nn.Sigmoid()(out_ctr), nn.Sigmoid()(out_cvr))
 
         return out_cvr, out_ctr, out_ctcvr
+
+
+class MF_AKBIPS_Exp(nn.Module):
+    def __init__(self, num_users, num_items, embedding_k=4, dataset_name="coat", *args, **kwargs):
+        super().__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.W = nn.Embedding(self.num_users, self.embedding_k)
+        self.H = nn.Embedding(self.num_items, self.embedding_k)
+        self.prediction_model = MF(
+            num_users = self.num_users, num_items = self.num_items,embedding_k=self.embedding_k, *args, **kwargs)
+        self.weight_model = MF(
+            num_users = self.num_users, num_items = self.num_items,embedding_k=self.embedding_k, *args, **kwargs)
+        if dataset_name == "coat":
+            self.epsilon = nn.Parameter(torch.rand(1,640)) 
+        elif dataset_name == "yahoo_r3":
+            self.epsilon = nn.Parameter(torch.rand(1,8192)) 
+
+    def get_embedding(self,x):
+        user_idx = x[:,0]
+        item_idx = x[:,1]
+        U_emb = self.W(user_idx)
+        V_emb = self.H(item_idx)
+        feature = torch.cat([U_emb ,V_emb ],dim=1)  
+        f_min = torch.min(feature)
+        f_max = torch.max(feature)
+        feature = feature - f_min / (f_max - f_min)                
+        feature = feature/feature.shape[1]
+
+        return feature
+
+    def exp_kernel(self,X,Y,gamma = 0.1):
+        Euclidean_distances = abs(torch.cdist(Y,X))
+        return torch.exp(-Euclidean_distances * gamma)
