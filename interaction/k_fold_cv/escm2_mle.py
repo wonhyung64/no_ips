87842890a5625c9138325882a0ddb9a87768cb18
WonHyung Shin <wonhyung64@icloud.com>
2025-01-06 14:07:52 +0900

    feat - rename

--- interaction/k_fold_cv/escm2_mle.py
+++ interaction/k_fold_cv/escm2_mle.py
@@ -86,7 +86,7 @@ for cv_num, (train_idx, test_idx) in enumerate(kf.split(x_train)):
     configs["device"] = device
     configs["cv_num"] = cv_num
     wandb_var = wandb.init(project="no_ips", config=configs)
-    wandb.run.name = f"cv_escm2_ips_{expt_num}"
+    wandb.run.name = f"cv_escm2_mle_{expt_num}"
 
     x_train = x_train_cv[train_idx]
     y_train = y_train_cv[train_idx]
