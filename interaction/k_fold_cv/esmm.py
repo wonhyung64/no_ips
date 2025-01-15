70d6a4519790d098784a91830557ae29616abad2
WonHyung Shin <wonhyung64@icloud.com>
2025-01-15 16:00:06 +0900

    feat - edit esmm

--- interaction/k_fold_cv/esmm.py
+++ interaction/k_fold_cv/esmm.py
@@ -88,8 +88,12 @@ for cv_num, (train_idx, test_idx) in enumerate(kf.split(x_train)):
     x_test = x_train_cv[test_idx]
     y_test = y_train_cv[test_idx]
 
-    num_sample = len(x_train)
-    total_batch = num_sample // batch_size
+    obs = sps.csr_matrix((np.ones(len(y_train)), (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
+    y_entire = sps.csr_matrix((y_train, (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
+    x_all = generate_total_sample(num_users, num_items)
+
+    num_samples = len(x_all)
+    total_batch = num_samples // batch_size
 
     # TRAIN
     model = ESMM(num_users, num_items, embedding_k)
@@ -97,17 +101,11 @@ for cv_num, (train_idx, test_idx) in enumerate(kf.split(x_train)):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     loss_fcn = torch.nn.BCELoss()
 
-    obs = sps.csr_matrix((np.ones(len(y_train)), (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
-    y_entire = sps.csr_matrix((y_train, (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
-    x_all = generate_total_sample(num_users, num_items)
-
     for epoch in range(1, num_epochs+1):
-        all_idx = np.arange(num_sample)
-        np.random.shuffle(all_idx)
-        model.train()
 
         ul_idxs = np.arange(x_all.shape[0]) # all
         np.random.shuffle(ul_idxs)
+        model.train()
 
         epoch_ctr_loss = 0.
         epoch_ctcvr_loss = 0.
