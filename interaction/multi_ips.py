ba5c066c105902cb56dea1f0ea51a21ae404930e
WonHyung Shin <wonhyung64@icloud.com>
2025-01-15 20:08:15 +0900

    feat - experiment for coat

--- interaction/multi_ips.py
+++ interaction/multi_ips.py
@@ -31,7 +31,7 @@ for seed in range(10):
     """coat"""
     parser.add_argument("--embedding-k", type=int, default=128)
     parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--weight-decay", type=float, default=1e-6)
+    parser.add_argument("--weight-decay", type=float, default=1e-4)
     parser.add_argument("--batch-size", type=int, default=4096)
     parser.add_argument("--dataset-name", type=str, default="coat")
 
--- interaction/snips.py
+++ interaction/snips.py
@@ -26,9 +26,9 @@ for seed in range(10):
     parser = argparse.ArgumentParser()
 
     """coat"""
-    parser.add_argument("--embedding-k", type=int, default=64)
-    parser.add_argument("--lr", type=float, default=1e-2)
-    parser.add_argument("--weight-decay", type=float, default=1e-4)
+    parser.add_argument("--embedding-k", type=int, default=128)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--weight-decay", type=float, default=1e-6)
     parser.add_argument("--batch-size", type=int, default=4096)
     parser.add_argument("--dataset-name", type=str, default="coat")
 
