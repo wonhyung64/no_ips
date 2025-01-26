21f62b0dffe3f96dfc5d6654327b6a6704947a2d
WonHyung Shin <wonhyung64@icloud.com>
2025-01-26 16:12:35 +0900

    Feat - yahoo setting

--- interaction/multi_ips.py
+++ interaction/multi_ips.py
@@ -37,7 +37,7 @@ for seed in range(10):
 
     """yahoo"""
     # parser.add_argument("--embedding-k", type=int, default=128)
-    # parser.add_argument("--lr", type=float, default=1e-3)
+    # parser.add_argument("--lr", type=float, default=1e-4)
     # parser.add_argument("--weight-decay", type=float, default=1e-4)
     # parser.add_argument("--batch-size", type=int, default=8192)
     # parser.add_argument("--dataset-name", type=str, default="yahoo_r3")
