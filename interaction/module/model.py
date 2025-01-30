5357bb30d2503d4f0094ea4ccd57aac43dac1eb2
WonHyung Shin <wonhyung64@icloud.com>
2025-01-30 19:55:20 +0900

    feat - add esmm

--- interaction/module/model.py
+++ interaction/module/model.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 
 
 class MF(nn.Module):
-    """The neural collaborative filtering method.
-    """
     def __init__(self, num_users, num_items, embedding_k):
         super(MF, self).__init__()
         self.num_users = num_users
@@ -21,6 +19,36 @@ class MF(nn.Module):
         out = torch.sum(user_embed.mul(item_embed), 1).unsqueeze(-1)
 
         return out, user_embed, item_embed
+
+
+class NCF(nn.Module):
+    def __init__(self, num_users, num_items, embedding_k):
+        super(NCF, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.user_embedding = nn.Embedding(self.num_users, self.embedding_k)
+        self.item_embedding = nn.Embedding(self.num_items, self.embedding_k)
+        self.ctr = nn.Sequential(
+            nn.Linear(self.embedding_k*2, self.embedding_k),
+            nn.ReLU(),
+            nn.Linear(self.embedding_k, 1, bias=False),
+        )
+        self.cvr = nn.Sequential(
+            nn.Linear(self.embedding_k*2, self.embedding_k),
+            nn.ReLU(),
+            nn.Linear(self.embedding_k, 1, bias=False),
+        )
+
+    def forward(self, x):
+        user_idx = x[:,0]
+        item_idx = x[:,1]
+        user_embed = self.user_embedding(user_idx)
+        item_embed = self.item_embedding(item_idx)
+        z_embed = torch.cat([user_embed, item_embed], axis=1)
+        ctr = self.ctr(z_embed)
+        cvr = self.cvr(z_embed)
+        return ctr, cvr
 
 
 class IpsV2(nn.Module):
@@ -58,7 +86,7 @@ class NCF1(nn.Module):
         return out, user_embed, item_embed
 
 
-class ESMM(nn.Module):
+class ESMM2(nn.Module):
     """ESMM"""
     def __init__(self, num_users, num_items, embedding_k):
         super(ESMM, self).__init__()
@@ -101,6 +129,39 @@ class ESMM(nn.Module):
         out_ctcvr = torch.mul(nn.Sigmoid()(out_ctr), nn.Sigmoid()(out_cvr))
 
         return out_cvr, out_ctr, out_ctcvr
+
+
+class ESMM(nn.Module):
+    """The neural collaborative filtering method.
+    """
+    def __init__(self, num_users, num_items, embedding_k):
+        super(NCF, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.embedding_k = embedding_k
+        self.user_embedding = nn.Embedding(self.num_users, self.embedding_k)
+        self.item_embedding = nn.Embedding(self.num_items, self.embedding_k)
+        self.ctr = nn.Sequential(
+            nn.Linear(self.embedding_k*2, self.embedding_k),
+            nn.ReLU(),
+            nn.Linear(self.embedding_k, 1, bias=False),
+        )
+        self.cvr = nn.Sequential(
+            nn.Linear(self.embedding_k*2, self.embedding_k),
+            nn.ReLU(),
+            nn.Linear(self.embedding_k, 1, bias=False),
+        )
+
+    def forward(self, x):
+        user_idx = x[:,0]
+        item_idx = x[:,1]
+        user_embed = self.user_embedding(user_idx)
+        item_embed = self.item_embedding(item_idx)
+        z_embed = torch.cat([user_embed, item_embed], axis=1)
+        ctr = self.ctr(z_embed)
+        cvr = self.cvr(z_embed)
+        ctcvr = torch.mul(nn.Sigmoid()(ctr), nn.Sigmoid()(cvr))
+        return cvr, ctr, ctcvr
 
 
 class MultiIps(nn.Module):
