623f2da3efbe448dc4e5efb1ba440b4e16aef5c7
WonHyung Shin <wonhyung64@icloud.com>
2025-02-02 17:38:26 +0900

    feat - finish

--- interaction/escm2_ips.py
+++ interaction/escm2_ips.py
@@ -41,7 +41,7 @@ parser.add_argument("--dataset-name", type=str, default="coat")
 # parser.add_argument("--dataset-name", type=str, default="yahoo_r3")
 
 parser.add_argument("--num-epochs", type=int, default=1000)
-parser.add_argument("--random-seed", type=int, default=seed)
+parser.add_argument("--random-seed", type=int, default=0)
 parser.add_argument("--evaluate-interval", type=int, default=50)
 parser.add_argument("--top-k-list", type=list, default=[1,3,5,7,10])
 parser.add_argument("--data-dir", type=str, default="./data")
