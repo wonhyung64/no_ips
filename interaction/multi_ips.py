89ed55415e899dff5a5f36b37bf5f93c08066b10
WonHyung Shin <wonhyung64@icloud.com>
2025-02-02 17:37:09 +0900

    feat - finish

--- interaction/multi_ips.py
+++ interaction/multi_ips.py
@@ -24,156 +24,155 @@ except:
     import wandb
 
 
-for seed in range(10):
-    # SETTINGS
-    parser = argparse.ArgumentParser()
+# SETTINGS
+parser = argparse.ArgumentParser()
 
-    """coat"""
-    parser.add_argument("--embedding-k", type=int, default=64)
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--weight-decay", type=float, default=1e-4)
-    parser.add_argument("--batch-size", type=int, default=4096)
-    parser.add_argument("--dataset-name", type=str, default="coat")
+"""coat"""
+parser.add_argument("--embedding-k", type=int, default=64)
+parser.add_argument("--lr", type=float, default=1e-3)
+parser.add_argument("--weight-decay", type=float, default=1e-4)
+parser.add_argument("--batch-size", type=int, default=4096)
+parser.add_argument("--dataset-name", type=str, default="coat")
 
-    """yahoo"""
-    # parser.add_argument("--embedding-k", type=int, default=64)
-    # parser.add_argument("--lr", type=float, default=1e-4)
-    # parser.add_argument("--weight-decay", type=float, default=1e-5)
-    # parser.add_argument("--batch-size", type=int, default=8192)
-    # parser.add_argument("--dataset-name", type=str, default="yahoo_r3")
+"""yahoo"""
+# parser.add_argument("--embedding-k", type=int, default=64)
+# parser.add_argument("--lr", type=float, default=1e-4)
+# parser.add_argument("--weight-decay", type=float, default=1e-5)
+# parser.add_argument("--batch-size", type=int, default=8192)
+# parser.add_argument("--dataset-name", type=str, default="yahoo_r3")
 
-    parser.add_argument("--num-epochs", type=int, default=1000)
-    parser.add_argument("--random-seed", type=int, default=seed)
-    parser.add_argument("--evaluate-interval", type=int, default=50)
-    parser.add_argument("--top-k-list", type=list, default=[1,3,5,7,10])
-    parser.add_argument("--data-dir", type=str, default="./data")
+parser.add_argument("--num-epochs", type=int, default=1000)
+parser.add_argument("--random-seed", type=int, default=0)
+parser.add_argument("--evaluate-interval", type=int, default=50)
+parser.add_argument("--top-k-list", type=list, default=[1,3,5,7,10])
+parser.add_argument("--data-dir", type=str, default="./data")
 
-    parser.add_argument("--G", type=int, default=1)
+parser.add_argument("--G", type=int, default=1)
 
-    try:
-        args = parser.parse_args()
-    except:
-        args = parser.parse_args([])
+try:
+    args = parser.parse_args()
+except:
+    args = parser.parse_args([])
 
-    embedding_k = args.embedding_k
-    lr = args.lr
-    weight_decay = args.weight_decay
-    batch_size = args.batch_size
-    num_epochs = args.num_epochs
-    random_seed = args.random_seed
-    evaluate_interval = args.evaluate_interval
-    top_k_list = args.top_k_list
-    data_dir = args.data_dir
-    dataset_name = args.dataset_name
-    G = args.G
+embedding_k = args.embedding_k
+lr = args.lr
+weight_decay = args.weight_decay
+batch_size = args.batch_size
+num_epochs = args.num_epochs
+random_seed = args.random_seed
+evaluate_interval = args.evaluate_interval
+top_k_list = args.top_k_list
+data_dir = args.data_dir
+dataset_name = args.dataset_name
+G = args.G
 
-    expt_num = f'{datetime.now().strftime("%y%m%d_%H%M%S_%f")}'
-    set_seed(random_seed)
-    device = set_device()
+expt_num = f'{datetime.now().strftime("%y%m%d_%H%M%S_%f")}'
+set_seed(random_seed)
+device = set_device()
 
-    # WANDB
-    configs = vars(args)
-    configs["device"] = device
-    wandb_var = wandb.init(project="no_ips", config=configs)
-    wandb.run.name = f"main_multi_ips_{expt_num}"
+# WANDB
+configs = vars(args)
+configs["device"] = device
+wandb_var = wandb.init(project="no_ips", config=configs)
+wandb.run.name = f"main_multi_ips_{expt_num}"
 
-    # DATA LOADER
-    x_train, x_test = load_data(data_dir, dataset_name)
-    x_train, y_train = x_train[:,:-1], x_train[:,-1]
-    x_test, y_test = x_test[:, :-1], x_test[:,-1]
+# DATA LOADER
+x_train, x_test = load_data(data_dir, dataset_name)
+x_train, y_train = x_train[:,:-1], x_train[:,-1]
+x_test, y_test = x_test[:, :-1], x_test[:,-1]
 
-    y_train = binarize(y_train)
-    y_test = binarize(y_test)
+y_train = binarize(y_train)
+y_test = binarize(y_test)
 
-    num_users = x_train[:,0].max()
-    num_items = x_train[:,1].max()
-    print(f"# user: {num_users}, # item: {num_items}")
+num_users = x_train[:,0].max()
+num_items = x_train[:,1].max()
+print(f"# user: {num_users}, # item: {num_items}")
 
-    obs = sps.csr_matrix((np.ones(len(y_train)), (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
-    y_entire = sps.csr_matrix((y_train, (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
-    x_all = generate_total_sample(num_users, num_items)
+obs = sps.csr_matrix((np.ones(len(y_train)), (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
+y_entire = sps.csr_matrix((y_train, (x_train[:, 0]-1, x_train[:, 1]-1)), shape=(num_users, num_items), dtype=np.float32).toarray().reshape(-1)
+x_all = generate_total_sample(num_users, num_items)
 
-    num_samples = len(x_all)
-    total_batch = num_samples // batch_size
+num_samples = len(x_all)
+total_batch = num_samples // batch_size
 
-    # TRAIN
-    model = SharedNCF(num_users, num_items, embedding_k)
-    model = model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    loss_fcn = torch.nn.BCELoss()
+# TRAIN
+model = SharedNCF(num_users, num_items, embedding_k)
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+loss_fcn = torch.nn.BCELoss()
 
-    for epoch in range(1, num_epochs+1):
+for epoch in range(1, num_epochs+1):
 
-        ul_idxs = np.arange(x_all.shape[0]) # all
-        np.random.shuffle(ul_idxs)
-        model.train()
+    ul_idxs = np.arange(x_all.shape[0]) # all
+    np.random.shuffle(ul_idxs)
+    model.train()
 
-        epoch_ctr_loss = 0.
-        epoch_cvr_loss = 0.
-        epoch_total_loss = 0.
+    epoch_ctr_loss = 0.
+    epoch_cvr_loss = 0.
+    epoch_total_loss = 0.
 
-        for idx in range(total_batch):
+    for idx in range(total_batch):
 
-            x_all_idx = ul_idxs[G*idx* batch_size : G*(idx+1)*batch_size]
-            x_sampled = x_all[x_all_idx]
-            x_sampled = torch.LongTensor(x_sampled).to(device)
-            sub_obs = torch.Tensor(obs[x_all_idx]).unsqueeze(-1).to(device)
-            sub_entire_y = torch.Tensor(y_entire[x_all_idx]).unsqueeze(-1).to(device)
+        x_all_idx = ul_idxs[G*idx* batch_size : G*(idx+1)*batch_size]
+        x_sampled = x_all[x_all_idx]
+        x_sampled = torch.LongTensor(x_sampled).to(device)
+        sub_obs = torch.Tensor(obs[x_all_idx]).unsqueeze(-1).to(device)
+        sub_entire_y = torch.Tensor(y_entire[x_all_idx]).unsqueeze(-1).to(device)
 
-            pred_cvr, pred_ctr, pred_ctcvr = model(x_sampled)
-            ctr_loss = loss_fcn(nn.Sigmoid()(pred_ctr), sub_obs)
-            cvr_loss = F.binary_cross_entropy(nn.Sigmoid()(pred_cvr), sub_entire_y, 1/(nn.Sigmoid()(pred_ctr).detach()))
-            total_loss = ctr_loss + cvr_loss
+        pred_cvr, pred_ctr, pred_ctcvr = model(x_sampled)
+        ctr_loss = loss_fcn(nn.Sigmoid()(pred_ctr), sub_obs)
+        cvr_loss = F.binary_cross_entropy(nn.Sigmoid()(pred_cvr), sub_entire_y, 1/(nn.Sigmoid()(pred_ctr).detach()))
+        total_loss = ctr_loss + cvr_loss
 
-            epoch_ctr_loss += ctr_loss
-            epoch_cvr_loss += cvr_loss
-            epoch_total_loss += total_loss
+        epoch_ctr_loss += ctr_loss
+        epoch_cvr_loss += cvr_loss
+        epoch_total_loss += total_loss
 
-            optimizer.zero_grad()
-            total_loss.backward()
-            optimizer.step()
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
 
-        print(f"[Epoch {epoch:>4d} Train Loss] rec: {epoch_total_loss.item():.4f}")
+    print(f"[Epoch {epoch:>4d} Train Loss] rec: {epoch_total_loss.item():.4f}")
 
-        loss_dict: dict = {
-            'epoch_ctr_loss': float(epoch_ctr_loss.item()),
-            'epoch_cvr_loss': float(epoch_cvr_loss.item()),
-            'epoch_total_loss': float(epoch_total_loss.item()),
-        }
+    loss_dict: dict = {
+        'epoch_ctr_loss': float(epoch_ctr_loss.item()),
+        'epoch_cvr_loss': float(epoch_cvr_loss.item()),
+        'epoch_total_loss': float(epoch_total_loss.item()),
+    }
 
-        wandb_var.log(loss_dict)
+    wandb_var.log(loss_dict)
 
-        if epoch % evaluate_interval == 0:
-            model.eval()
-            x_test_tensor = torch.LongTensor(x_test-1).to(device)
-            pred_, _, __ = model(x_test_tensor)
-            pred = pred_.flatten().cpu().detach().numpy()
+    if epoch % evaluate_interval == 0:
+        model.eval()
+        x_test_tensor = torch.LongTensor(x_test-1).to(device)
+        pred_, _, __ = model(x_test_tensor)
+        pred = pred_.flatten().cpu().detach().numpy()
 
-            ndcg_res = ndcg_func(pred, x_test, y_test, top_k_list)
-            ndcg_dict: dict = {}
-            for top_k in top_k_list:
-                ndcg_dict[f"ndcg_{top_k}"] = np.mean(ndcg_res[f"ndcg_{top_k}"])
+        ndcg_res = ndcg_func(pred, x_test, y_test, top_k_list)
+        ndcg_dict: dict = {}
+        for top_k in top_k_list:
+            ndcg_dict[f"ndcg_{top_k}"] = np.mean(ndcg_res[f"ndcg_{top_k}"])
 
-            recall_res = recall_func(pred, x_test, y_test, top_k_list)
-            recall_dict: dict = {}
-            for top_k in top_k_list:
-                recall_dict[f"recall_{top_k}"] = np.mean(recall_res[f"recall_{top_k}"])
+        recall_res = recall_func(pred, x_test, y_test, top_k_list)
+        recall_dict: dict = {}
+        for top_k in top_k_list:
+            recall_dict[f"recall_{top_k}"] = np.mean(recall_res[f"recall_{top_k}"])
 
-            ap_res = ap_func(pred, x_test, y_test, top_k_list)
-            ap_dict: dict = {}
-            for top_k in top_k_list:
-                ap_dict[f"ap_{top_k}"] = np.mean(ap_res[f"ap_{top_k}"])
+        ap_res = ap_func(pred, x_test, y_test, top_k_list)
+        ap_dict: dict = {}
+        for top_k in top_k_list:
+            ap_dict[f"ap_{top_k}"] = np.mean(ap_res[f"ap_{top_k}"])
 
-            auc = roc_auc_score(y_test, pred)
+        auc = roc_auc_score(y_test, pred)
 
-            wandb_var.log(ndcg_dict)
-            wandb_var.log(recall_dict)
-            wandb_var.log(ap_dict)
-            wandb_var.log({"auc": auc})
+        wandb_var.log(ndcg_dict)
+        wandb_var.log(recall_dict)
+        wandb_var.log(ap_dict)
+        wandb_var.log({"auc": auc})
 
-    print(f"NDCG: {ndcg_dict}")
-    print(f"Recall: {recall_dict}")
-    print(f"AP: {ap_dict}")
-    print(f"AUC: {auc}")
+print(f"NDCG: {ndcg_dict}")
+print(f"Recall: {recall_dict}")
+print(f"AP: {ap_dict}")
+print(f"AUC: {auc}")
 
-    wandb.finish()
+wandb.finish()
