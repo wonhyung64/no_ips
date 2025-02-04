fe648751773763801acec6ed5265cf34609e6fc3
WonHyung Shin <wonhyung64@icloud.com>
2025-02-04 22:36:41 +0900

    feat - add single task

--- causality/module/dataset.py
+++ causality/module/dataset.py
@@ -4,45 +4,25 @@ import pandas as pd
 
 
 def load_data(data_dir, dataset_name):
-    dataset_dir = os.path.join(data_dir, dataset_name)
+    if dataset_name == "original":
+        sub_dir = f"dunn_cat_mailer_10_10_1_1/original_rp0.40"
+    elif dataset_name == "personalized":
+        sub_dir = f"dunn_cat_mailer_10_10_1_1/rank_rp0.40_sf2.00_nr210"
 
-    if dataset_name == "yahoo_r3":
-        train_file = os.path.join(dataset_dir, "ydata-ymusic-rating-study-v1_0-train.txt")
-        test_file = os.path.join(dataset_dir, "ydata-ymusic-rating-study-v1_0-test.txt")
-        x_train = []
-        with open(train_file, "r") as f:
-            for line in f:
-                x_train.append(line.strip().split())
-        x_train = np.array(x_train).astype(int)
-        x_test = []
-        with open(test_file, "r") as f:
-            for line in f:
-                x_test.append(line.strip().split())
-        x_test = np.array(x_test).astype(int)
+    dataset_dir = os.path.join(data_dir, sub_dir)
 
-    elif dataset_name == "coat":
-        train_file = os.path.join(dataset_dir, "train.csv")
-        test_file = os.path.join(dataset_dir, "test.csv")
+    train_df = pd.read_csv(f"{dataset_dir}/data_vali.csv")
+    test_df = pd.read_csv(f"{dataset_dir}/data_test.csv")
 
-        x_train = pd.read_csv(train_file).to_numpy()
-        x_train = np.stack([x_train[:,0]+1, x_train[:,1]+1, x_train[:,2]], axis=-1)
-
-        x_test = pd.read_csv(test_file).to_numpy()
-        x_test = np.stack([x_test[:,0]+1, x_test[:,1]+1, x_test[:,2]], axis=-1)
+    x_train = train_df[["idx_user", "idx_item", "outcome", "treated", "propensity"]].to_numpy()
+    x_test = test_df.groupby(["idx_user", "idx_item"]).mean("causal_effect")["causal_effect"].reset_index().to_numpy()
 
     print(f"Loaded from {dataset_name} dataset")
     print("[train] num data:", x_train.shape[0])
     print("[test]  num data:", x_test.shape[0])
-    print("user, item indices start from '1'.")
+    print("user, item indices start from '0'.")
 
     return x_train, x_test
-
-
-def binarize(y, thres=3):
-    y[y< thres] = 0
-    y[y>=thres] = 1
-
-    return y
 
 
 def generate_total_sample(num_user, num_item):
--- causality/module/metric.py
+++ causality/module/metric.py
@@ -2,6 +2,33 @@ import numpy as np
 from collections import defaultdict
 
 
+def ncdcg_func(pred, x_test, y_test, top_k_list):
+    """
+    Evaluate cDCG@K of the trained model on test dataset.
+    """
+    all_user_idx = np.unique(x_test[:,0])
+    all_test_idx = np.arange(len(x_test))
+    result_map = defaultdict(list)
+
+    for user_idx in all_user_idx:
+        item_idx = all_test_idx[x_test[:, 0] == user_idx]
+        pred_u = pred[item_idx]
+        true_u = y_test[item_idx]
+
+        for top_k in top_k_list:
+            if len(true_u) < top_k:
+                break
+            log2_iplus1 = np.log2(1+np.arange(1,top_k+1))
+            pred_top_k_rel = true_u[np.argsort(-pred_u)][:top_k]
+            true_top_k_rel = true_u[np.argsort(-true_u)][:top_k]
+            cdcg_k = pred_top_k_rel / log2_iplus1
+            icdcg_k = true_top_k_rel / log2_iplus1
+            ncdcg_k = np.sum(cdcg_k) / (np.sum(icdcg_k)+1e-9)
+            result_map[f"ncdcg_{top_k}"].append(ncdcg_k)
+
+    return result_map
+
+
 def cdcg_func(pred, x_test, y_test, top_k_list):
     """
     Evaluate cDCG@K of the trained model on test dataset.
@@ -20,7 +47,7 @@ def cdcg_func(pred, x_test, y_test, top_k_list):
                 break
             log2_iplus1 = np.log2(1+np.arange(1,top_k+1))
             pred_top_k_rel = true_u[np.argsort(-pred_u)][:top_k]
-            cdcg_k = (2**pred_top_k_rel-1) / log2_iplus1
+            cdcg_k = pred_top_k_rel / log2_iplus1
             result_map[f"cdcg_{top_k}"].append(cdcg_k)
 
     return result_map
