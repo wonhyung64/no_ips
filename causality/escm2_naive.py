5acb58b6e9bb7b4ae9a6685c256a1d8d7d0b47ea
WonHyung Shin <wonhyung64@icloud.com>
2025-02-17 20:55:26 +0900

    feat - hyperparams setting

--- causality/escm2_naive.py
+++ causality/escm2_naive.py
@@ -28,7 +28,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dataset-name", type=str, default="original")#[original, personalized]
 parser.add_argument("--lr1", type=float, default=1e-3)
 parser.add_argument("--weight-decay1", type=float, default=1e-4)
-parser.add_argument("--lr0", type=float, default=1e-4)
+parser.add_argument("--lr0", type=float, default=1e-3)
 parser.add_argument("--weight-decay0", type=float, default=1e-4)
 
 
