9f36d9a1615ed7c199ffff14200a1005e84e0567
WonHyung Shin <wonhyung64@icloud.com>
2025-04-10 21:27:47 +0900

    feat - add scatter plot

--- causality/visualize_ps_kde.py
+++ causality/visualize_ps_kde.py
@@ -71,6 +71,7 @@ ps_model_name_list = ["Multi-IPS", "ESCM2-IPS"]
 ps_model_name_list = ["multi", "escm2"]
 labels_list = [["Y(1)", "Y(0)", "Plus", "True"], ["Y(1)", "Y(0)", "Y(1) + Y(0)", "True"]]
 density_list = []
+
 for ps_model_name in ps_model_name_list:
     ps_pred_y1 = np.load(f"{data_dir}/pred_t_{ps_model_name}_ips_y1_{dataset_name[:3]}_seed{random_seed}.npy")
     ps_pred_y0 = np.load(f"{data_dir}/pred_t_{ps_model_name}_ips_y0_{dataset_name[:3]}_seed{random_seed}.npy")
@@ -80,14 +81,15 @@ for ps_model_name in ps_model_name_list:
     data_arr = np.array([ps_pred_y1, ps_pred_y0, ps_pred_plus, ps_true])
     x_min_max = (data_arr.min(), data_arr.max())
 
-    if dataset_name == "personalized":
-        x_range = np.linspace(-8, 14, 1000)
-    else:
+    if dataset_name == "original":
         x_range = np.linspace(x_min_max[0], x_min_max[1], 1000)
+    elif dataset_name == "personalized":
+        x_range = np.linspace(-8, 14, 1000)
 
     file_name = f"{data_dir}/density_t_{ps_model_name}_ips_{dataset_name[:3]}_seed{random_seed}.npy"
     if os.path.exists(file_name):
         density = np.load(file_name)
+        
     else:
         density = []
         for data in data_arr:
@@ -96,8 +98,7 @@ for ps_model_name in ps_model_name_list:
         np.save(f"{data_dir}/density_t_{ps_model_name}_ips_{dataset_name[:3]}_seed{random_seed}.npy", np.array(density), allow_pickle=True)
 
     density_list.append(density)
-plt.hist(density_list[0][-1].mean())
-plt.hist(density_list[1][-1].mean())
+
 
 # %%
 # VISUALIZATION OPTIONS
@@ -123,11 +124,10 @@ for i, (densities, labels, ax) in enumerate(zip(density_list, labels_list, axes)
             labels.append(label)
 
         ax.set_ylim(0, 0.5)
-        # ax.set_xlim(np.floor(x_min_max[0]), np.ceil(x_min_max[1]))
         if dataset_name == "original": 
-            ax.set_xlim(-8, 4)
-        else:
             ax.set_xlim(np.floor(x_min_max[0]), np.ceil(x_min_max[1]))
+        else:
+            ax.set_xlim(-8, 14)
 
         ax.tick_params(axis='x', which='both', top=False, labelsize=font_size)
         ax.tick_params(axis='y', which='both', labelsize=font_size)
