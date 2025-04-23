1e46023e3d97ddeb951ac80050bb35477aca15ac
WonHyung Shin <wonhyung64@icloud.com>
2025-04-23 13:12:07 +0900

    feat - recon

--- interaction/k_fold_cv/multi.py
+++ interaction/k_fold_cv/multi.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import KFold
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from module.model import SharedNCF
+from module.model import SharedNCF, SharedMF
 from module.metric import ndcg_func, recall_func, ap_func
 from module.dataset import binarize, load_data, generate_total_sample
 from module.utils import set_device, set_seed
