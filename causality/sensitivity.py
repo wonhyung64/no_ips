82fb716effbac2e6a41dd211548f3280fb0e6307
WonHyung Shin <wonhyung64@icloud.com>
2025-04-28 12:43:14 +0900

    feat - recon

--- causality/sensitivity.py
+++ causality/sensitivity.py
@@ -76,6 +76,9 @@ print(f"# user: {num_users}, # item: {num_items}")
 kf = KFold(n_splits=4, shuffle=True, random_state=random_seed)
 for cv_num, (train_idx, test_idx) in enumerate(kf.split(x_train)):
 
+    if (alpha == 0.01) & (cv_num < 2):
+        continue
+    
     configs = vars(args)
     configs["device"] = device
     configs["cv_num"] = cv_num
